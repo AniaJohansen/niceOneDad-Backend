795f2c6a08c60241e464e9fa7b7baca3240329f0
aniajohansen <ania.johansen@payex.com>
2020-04-21 23:13:03 +0200

    Add project files.

---
+++ src/MyFirstTestAPI/Controllers/JokesController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MyFirstTestAPI.Controllers
+{
+    [Route("jokes")]
+    [ApiController]
+    public class JokesController : ControllerBase
+    {
+        // GET jokes/
+        [HttpGet]
+        public async Task<ActionResult<string>>  Get()
+        {
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Accept","text/plain");
+            HttpResponseMessage response = await client.GetAsync("https://icanhazdadjoke.com/");
+           
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        //// GET jokes/5
+        //[HttpGet("{id}")]
+        //public async Task <ActionResult<string>> Get(string id)
+        //{
+        //    var client = new HttpClient();
+        //    //client.DefaultRequestHeaders
+        //    HttpResponseMessage response = await client.GetAsync($"https://icanhazdadjoke.com/j/{id}");
+        //    return await response.Content.ReadAsStringAsync();
+        //}
+
+        // GET jokes/search
+        // Let's you specify a searchterm in the URL
+        [HttpGet("{searchTerm}")]
+        public async Task<ActionResult<string>> Get(string searchTerm)
+        {
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Accept","text/plain");
+            HttpResponseMessage response = await client.GetAsync($"https://icanhazdadjoke.com/search?term={searchTerm}");
+
+            return await response.Content.ReadAsStringAsync();
+        
+        }
+
+
+    }
+}
---
+++ src/MyFirstTestAPI/Controllers/ValuesController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstTestAPI.Controllers
+{
+    [Route("values")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET values/
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value " + $"{id + 3}";
+        }
+
+        [HttpGet("{name}")]
+        public ActionResult<string> Get(string name)
+        {
+            return "Name requested is" + $"{name}";
+        }
+
+        // POST values/
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
